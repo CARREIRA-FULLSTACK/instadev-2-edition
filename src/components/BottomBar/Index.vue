3de2b450c2013ec0552d7bbc1dc36baf2fa90391
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-08-13 10:02:47 -0300

    add feature to navigation and avatar in bottomBar

--- src/components/BottomBar/Index.vue
+++ src/components/BottomBar/Index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="full-width row justify-around">
-    <q-img width="24px" src="../../assets/house.svg" ></q-img>
+    <q-img  @click="goTo('main')" width="24px" src="../../assets/house.svg" ></q-img>
     <q-img width="30px" src="../../assets/search.svg" ></q-img>
     <q-img width="30px" src="../../assets/add.svg" ></q-img>
     <q-img width="34px" src="../../assets/heart.svg" ></q-img>
     <q-avatar size="28px" class="cursor-pointer" @click="goTo('my-area')">
-      <img src="https://cdn.quasar.dev/img/avatar.png">
+      <img :src="user.avatar || 'https://static.wikia.nocookie.net/caramella-girls/images/9/99/Blankpfp.png/revision/latest?cb=20190122015011'">
     </q-avatar>
   </div>
 </template>
@@ -13,9 +13,20 @@
 <script>
 export default {
   name: 'BottomBar',
+  data() {
+    return {
+      user: {},
+    };
+  },
+  mounted() {
+    this.loadProfileData();
+  },
   methods: {
     goTo(route) {
       this.$router.push({ path: route });
+    },
+    loadProfileData() {
+      this.user = this.$store.getters['user/getUserData'];
     },
   },
 };
